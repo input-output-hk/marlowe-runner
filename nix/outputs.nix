ef2c0c38088760ce9e7391bb0a8b0b08d1dd221c
zeme <lorenzo.calegari@iohk.io>
2023-09-29 15:40:07 +0200

    wip

--- nix/outputs.nix
+++ nix/outputs.nix
@@ -6,8 +6,6 @@
 
     packages.marlowe-runner = repoRoot.nix.marlowe-runner;
 
-    packages.marlowe-runner2 = repoRoot.nix.marlowe-runner2;
-
     operables = repoRoot.nix.marlowe-runner.deploy.operables;
 
     oci-images = repoRoot.nix.marlowe-runner.deploy.oci-image;
