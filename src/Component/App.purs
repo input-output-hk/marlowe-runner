d5fd922d8153b2173b5e54376c61132ecd588624
Tomasz Rybarczyk <tomasz.rybarczyk@iohk.io>
2023-10-13 13:11:45 +0200

    Minor styling fixes

--- src/Component/App.purs
+++ src/Component/App.purs
@@ -357,7 +357,7 @@ mkApp = do
                 [ DOM.div
                     { className: "col-3 pt-3 pb-3 background-color-primary-light", id: "marlowe-logo-container" } $
                     DOM.a { href: "#" } [ svgImg { src: marloweLogoUrl } ]
-                , DOM.div { className: "col-9 pt- pb-2 bg-white" } $ DOM.ul { className: "list-unstyled d-flex justify-content-end" } $
+                , DOM.div { className: "col-9 pt-3 pb-2 bg-white" } $ DOM.ul { className: "list-unstyled d-flex justify-content-end" } $
                     [ DOM.li {} $ ReactContext.consumer msgHubProps.ctx \msgs ->
                         [ linkWithIcon
                             { icon: unsafeIcon "h5"
