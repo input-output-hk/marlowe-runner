eb44d05f96a986a78afcb9addc60c7760f5c8e61
Yves Hauser <yves.hauser@gmail.com>
2023-10-24 13:09:29 +0200

    PLT-8193: Nami configuration

---
+++ e2e/src/features/change-wallet.feature
@@ -0,0 +1,13 @@
+@smoke
+@regression
+
+Feature: As a user, I would like to switch the wallet
+
+    As a user I would like to switch the wallet
+
+    @dev
+    Scenario: Install Nami
+      Given I configure my nami wallet
+      And I am on the "home" page
+      Then I should see a "heading" with "Choose a wallet" text
+
--- e2e/src/step-definitions/wallet-config/namiWallet.ts
+++ e2e/src/step-definitions/wallet-config/namiWallet.ts
@@ -3,6 +3,8 @@ import { When } from '@cucumber/cucumber';
 import { ScenarioWorld } from '../setup/world.js';
 import { waitFor } from "../../support/wait-for-behavior.js";
 import { testWallet } from "../../support/walletConfiguration.js";
+import { inputValue } from '../../support/html-behavior.js';
+import * as fs from 'fs';
 
 function sleep(seconds: number): Promise<void> {
   return new Promise(resolve => setTimeout(resolve, seconds * 1000));
@@ -68,27 +70,61 @@ When(
       globalStateManager
     } = this;
 
+    const mnemonic = fs.readFileSync('artifacts/mnemonic.txt', 'utf-8');
+    const words = mnemonic.trim().split(' ');
 
-    const EXTENSION_URL = 'chrome-extension://gafhhkghbfjjkeiendhlofajokpaflmk';
+    const EXTENSION_URL = 'chrome-extension://aopgffgkiplghkmanlffiacfjokdigob';
+    const newPage = await page.context().newPage();
+    await newPage.goto(`${EXTENSION_URL}/createWalletTab.html?type=import&length=24`);
 
-    await page.goto(`${EXTENSION_URL}/app.html`);
-    await page.addInitScript((testWallet) => {
-      window.localStorage.setItem('lock', testWallet.lock);
-      window.localStorage.setItem('analyticsAccepted', testWallet.analyticsAccepted);
-      window.localStorage.setItem('showDappBetaModal', testWallet.showDappBetaModal);
-      window.localStorage.setItem('wallet', testWallet.wallet);
-      const keyAgentData = JSON.parse(testWallet?.backgroundStorage?.keyAgentsByChain);
-      const mnemonicData = JSON.parse(testWallet?.backgroundStorage?.mnemonic);
-      const backgroundStorage = {
-        mnemonic: mnemonicData,
-        keyAgentsByChain: keyAgentData,
-        MIGRATION_STATE: { state: 'up-to-date' }
-      };
-      window.localStorage.setItem('BACKGROUND_STORAGE', JSON.stringify(backgroundStorage));
-      window.localStorage.setItem('appSettings', testWallet.appSettings);
-      window.localStorage.setItem('keyAgentData', testWallet.keyAgentData);
-    }, testWallet);
-    await page.goto(`${EXTENSION_URL}/app.html`);
-    await page.waitForTimeout(5000);
+    const inputField = async (name, value) => {
+      const locator = await newPage.getByRole("textbox", { name: name, exact: true });
+      const result = await locator.isVisible();
+      if (result) {
+        await inputValue(locator, value);
+        return result;
+      }
+    }
+
+    for (let i = 0; i < 24; i++) {
+      await inputField(`Word ${i+1}`, words[i]);
+    }
+
+    await waitFor(async() => {
+      const buttonName = "Next"
+      const locator = await newPage.getByRole("button", { name: buttonName, exact: true });
+      const result = await locator.isVisible();
+      if (result) {
+        await locator.click();
+        return result;
+      }
+    });
+
+    await inputField("Enter account name", "Runner test");
+    await inputField("Enter password", "Runner test");
+    await inputField("Confirm password", "Runner test");
+
+    await waitFor(async() => {
+      const buttonName = "Create"
+      const locator = await newPage.getByRole("button", { name: buttonName, exact: true });
+      const result = await locator.isVisible();
+      if (result) {
+        await locator.click();
+        return result;
+      }
+    });
+
+    await newPage.waitForTimeout(2000);
+
+    await waitFor(async() => {
+      const buttonName = "Close"
+      const locator = await newPage.getByRole("button", { name: buttonName, exact: true });
+      const result = await locator.isVisible();
+      if (result) {
+        await locator.click();
+        return result;
+      }
+    });
+
     await page.reload();
 });
